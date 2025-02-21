dde2bd47dfe7af770c202dfb3e2f6ac936760c10
Alejandro <alejandrorcc24@gmail.com>
2025-02-21 20:44:55 +0000

    finished

--- src/js/components/Player.jsx
+++ src/js/components/Player.jsx
@@ -1,63 +1,70 @@
 import React, { useRef } from "react";
-
-
-export const Player = ({ currentSong, isPlaying, setIsPlaying, next, prev, audioElem, random }) => {
-
-    const timeStamp = useRef()
-
-    const PlayPause = () => {
+export const Player = ({
+    audioElem,
+    isPlaying,
+    setIsPlaying,
+    currentSong,
+    next,
+    prev,
+    random,
+}) => {
+    const timeStamp = useRef();
+    const playPause = () => {
         if (!isPlaying && !currentSong) {
-            random()
-            setIsPlaying(!isPlaying);
+            random();
         } else {
-
             setIsPlaying(!isPlaying);
         }
-    }
-
-
+    };
     const setTime = (e) => {
-        const width = timeStamp.current.clientWidth;
+        let width = timeStamp.current.clientWidth;
         const offset = e.nativeEvent.offsetX;
-        const progress = (offset / width) * 100
-        audioElem.current.currentTime = (progress / 100) * currentSong.length
-    }
-
-
+        const progress = (offset / width) * 100;
+    };
     return (
-        <div className="container w-75 d-flex-column justify-content-center align-items-center">
-            <section>
-                <h2 className="text-white">{currentSong && currentSong.name}</h2>
-            </section>
-            <section>
-                <div className="navigation" onClick={setTime} ref={timeStamp}>
-                    {currentSong ?
-                        <div className="seeker" style={{ width: `${currentSong.progress}%` }}>
-                        </div>
-                        :
-                        ""
-                    }
+        <div className="container mr-3 rounded border p-2 w-75">
+            <div className="card player-wrapper border bg-dark text-white border-primary-subtle">
+                <div className="d-flex justify-content-center">
+                    <h2 className="my-5">
+                        {currentSong && currentSong.name ? currentSong.name : ""}
+                    </h2>
                 </div>
-            </section>
-            <div className="d-flex justify-content-center">
-
-                <div className="mx-2">
-                    <span onClick={prev}>
-                        <i class="fa-solid fa-backward-step"></i>
-                    </span>
-                </div>
-
-                <div className="mx-2">
-                    <span onClick={PlayPause}>
-                        <i class={`${isPlaying ? "fa-solid fa-pause fa-2xl" : "fa-solid fa-play fa-2xl"}`} />
-                    </span>
-                </div>
-                <div className="mx-2">
-                    <span onClick={next}>
-                        <i class="fa-solid fa-forward-step"></i>
-                    </span>
+                <div className="card-body ">
+                    <div className="navigation" onClick={setTime} ref={timeStamp}>
+                        {currentSong ? (
+                            <div
+                                className="seekBar"
+                                style={{ width: `${currentSong.progress}%` }}
+                            ></div>
+                        ) : (
+                            ""
+                        )}
+                    </div>
                 </div>
             </div>
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="border mx-1 rounded-5 h-50">
+                    <span
+                        onClick={prev}
+                        className="fa-solid fa-backward text-white mx-3"
+                    ></span>
+                </div>
+                <div className="play border mx-1 rounded-5 p-2 text-white">
+                    <span
+                        onClick={playPause}
+                        className={`mx-3  ${isPlaying ? "fa-solid fa-pause" : "fa-solid fa-play "
+                            }`}
+                    ></span>
+                </div>
+                <div className="border mx-1 rounded-5 h-50">
+                    <span
+                        onClick={next}
+                        className="text-white mx-3 fa-solid fa-forward"
+                    ></span>
+                </div>
+
+
+            </div>
         </div>
-    )
-}
+    );
+};
--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -1,130 +1,106 @@
-import React, { useEffect, useState } from "react";
 
+import React, { useEffect, useState, useRef } from "react";
 import { Player } from "./Player";
 
+
+
+//create your first component
 const Home = () => {
-
 	const [songs, setSongs] = useState([]);
-	const [currentSong, setCurrentSong] = useState();
 	const [isPlaying, setIsPlaying] = useState(false);
-	// const [next, setNext] = useState();
-	// const [prev, setPrev] = useState();
-
-	const audioElem = useState();
-
+	const [currentSong, setCurrentSong] = useState();
+	const audioElem = useRef();
 
 	useEffect(() => {
-		getData()
-	}, [])
+		getSongs();
+	}, []);
 
 	useEffect(() => {
 		isPlaying ? audioElem.current.play() : audioElem.current.pause();
-	}, [isPlaying])
+	}, [isPlaying]);
 
-	const getData = async () => {
-
+	const getSongs = async () => {
 		try {
-			const resp = await fetch("https://playground.4geeks.com/sound/songs")
-			const data = await resp.json()
-			setSongs(data)
+			const resp = await fetch("https://playground.4geeks.com/sound/songs");
+			const data = await resp.json();
+			setSongs(await data);
 		} catch (error) {
+			console.error("error----> ", error);
 		}
-	}
-
-
-	const handleSelectedSong = (el) => {
-		setCurrentSong(el)
-	}
-
-	const getIndex = () => {
-		const aux = songs.sound.filter((el) => el.id === currentSong.id)
-		return songs.sound.indexOf(aux[0])
-	}
-
-	const next = () => {
-		shuffle ?
-			random()
-			:
-			songs.sound.length === getIndex() + 1
-				?
-				setCurrentSong(songs.sound[0])
-				:
-				setCurrentSong(songs.sound[getIndex() + 1])
-		setIsPlaying(true)
-	}
-
-	const prev = () => {
-		0 === getIndex() ?
-			setCurrentSong(songs.sound[songs.sound.length - 1])
-			:
-			setCurrentSong(songs.sound[getIndex() - 1])
-		setIsPlaying(true)
-	}
-
-
-	const random = () => {
-		setCurrentSong(songs.sound[Math.floor(Math.random() * songs.sound.length)])
-		setIsPlaying(true)
-	}
+	};
 
 	const onPlaying = () => {
 		const duration = audioElem.current.duration;
 		const current = audioElem.current.currentTime;
 		setCurrentSong({
 			...currentSong,
-			"progress": (current / duration) * 100,
+			progress: (current / duration) * 100,
 			length: duration,
-			current: current
-		})
-	}
+			current: current,
+		});
+	};
 
+	const getSongIndex = () => {
+		const aux = songs.sound.filter((el) => el.id === currentSong.id);
+		return songs.sound.indexOf(aux[0]);
+	};
 
+	const prev = () => {
+		0 === getSongIndex()
+			? setCurrentSong(songs.sound[songs.sound.length - 1])
+			: setCurrentSong(songs.sound[getSongIndex() - 1]);
+		setIsPlaying(true);
+	};
 
+	const handleSelectedSong = (i) => {
+		setCurrentSong(songs.sound[i]);
+		setIsPlaying(true);
+	};
+
+	const random = () => {
+		setCurrentSong(songs.sound[Math.floor(Math.random() * songs.sound.length)]);
+		setIsPlaying(true);
+	};
 
 	return (
-		<section className="text-center container bg-dark test-white d-flex align-items-center row">
+		<div className="App bg-dark overflow-hidden">
+			<div className="d-flex  align-items-center row">
+				<div className="container overflow-y-auto playlist mr-3 col-sm-12 col-md-2 col-lg-2">
+					<ul className="list-group ">
+						{songs &&
+							songs.sound?.map((el, i) => (
+								<li className={`list-group-item item text-white border border-primary-subtle rounded-0 ${el.name == currentSong?.name ? "active" : ""}`} key={i} onClick={() => handleSelectedSong(i)}>
 
-			<section className="d-flex container col-2">
-				<div className="playlist overflow-y-auto">
-					<ul className="list-group">
-						{songs.sound?.map((el, i) => <li className={`list-group-item dg-dark ${currentSong && el.name === currentSong.name ? "active" : ""}`} key={i} onClick={() => handleSelectedSong(songs.sound[i])}>{el.name}</li>)}
+									{el.name}
+								</li>
+							))}
 					</ul>
 				</div>
+				<div className="container col-sm-12 col-md-8 col-lg-8">
+					
+					<audio
+						hidden
+						src={
+							currentSong && "https://playground.4geeks.com/sound/songs" + currentSong.url}
+						ref={audioElem}
+						onTimeUpdate={onPlaying}
+						autoPlay
+					/>
 
-			</section>
-
-
-
-
-			<section className="container de-flex col-10">
-				<audio
-					hidden
-					src={currentSong && "https://playground.4geeks.com/apis/fake/sound/" + currentSong.url}
-					ref={audioElem}
-					autoPlay
-					onPlaying={onPlaying}
-					onEnded={next}
-					onTimeUpdate={onPlaying}
-				/>
-				<player
-					currentSong={currentSong}
-					isPlaying={isPlaying}
-					setIsPlaying={setIsPlaying}
-					audioElem={audioElem}
-					setSong={setSongs}
-					next={next}
-					prev={prev}
-					random={random}
-
-
-
-				/>
-			</section>
-
-
-
-		</section>
+					<Player
+						songs={songs}
+						setSongs={setSongs}
+						isPlaying={isPlaying}
+						setIsPlaying={setIsPlaying}
+						audioElem={audioElem}
+						currentSong={currentSong}
+						prev={prev}
+						random={random}
+					/>
+				</div>
+			</div>
+		</div>
 	);
-};
+}
 
 export default Home;
